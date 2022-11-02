542aba187b97c9e4ca6923c005b38ed7974b7d90
David Osemwota <hirakoshinji722@gmail.com>
2022-11-02 12:17:32 +0100

    Update app colours

--- app/src/main/java/io/osemwota/bankd/ui/theme/Color.kt
+++ app/src/main/java/io/osemwota/bankd/ui/theme/Color.kt
@@ -2,7 +2,45 @@ package io.osemwota.bankd.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Blue10 = Color(0xFF150a29)
+val Blue20 = Color(0xFF2a1452)
+val Blue30 = Color(0xFF3f1f7a)
+val Blue40 = Color(0xFF5429a3)
+val Blue80 = Color(0xFFc3adeb)
+val Blue90 = Color(0xFFe1d6f5)
+
+val DarkBlue10 = Color(0xFF001133)
+val DarkBlue20 = Color(0xFF002266)
+val DarkBlue30 = Color(0xFF003399)
+val DarkBlue40 = Color(0xFF0044cc)
+val DarkBlue80 = Color(0xFF99bbff)
+val DarkBlue90 = Color(0xFFccddff)
+
+val Purple10 = Color(0xFF330033)
+val Purple20 = Color(0xFF660066)
+val Purple30 = Color(0xFF990099)
+val Purple40 = Color(0xFFcc00cc)
+val Purple80 = Color(0xFFff99ff)
+val Purple90 = Color(0xFFffccff)
+
+val Grey10 = Color(0xFF1a1a1a)
+val Grey20 = Color(0xFF333333)
+val Grey90 = Color(0xFFe6e6e6)
+val Grey95 = Color(0xFFf2f2f2)
+val Grey99 = Color(0xFFFBFDFD)
+
+internal val Red10 = Color(0xFF410001)
+internal val Red20 = Color(0xFF680003)
+internal val Red30 = Color(0xFF930006)
+internal val Red40 = Color(0xFFBA1B1B)
+internal val Red80 = Color(0xFFFFB4A9)
+internal val Red90 = Color(0xFFFFDAD4)
+
+internal val Green10 = Color(0xFF003312)
+internal val Green20 = Color(0xFF006624)
+internal val Green30 = Color(0xFF009936)
+internal val Green40 = Color(0xFF00cc47)
+internal val Green50 = Color(0xFF00ff59)
+internal val Green80 = Color(0xFF99ffbd)
+internal val Green90 = Color(0xFFccffde)
+
