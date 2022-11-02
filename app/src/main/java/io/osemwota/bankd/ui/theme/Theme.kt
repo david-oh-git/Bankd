395ab144daae9643dff252a717cf4055138762cb
David Osemwota <hirakoshinji722@gmail.com>
2022-11-02 12:17:10 +0100

    Switch to material3

--- app/src/main/java/io/osemwota/bankd/ui/theme/Theme.kt
+++ app/src/main/java/io/osemwota/bankd/ui/theme/Theme.kt
@@ -1,43 +1,86 @@
 package io.osemwota.bankd.ui.theme
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+private val DarkColorPalette = darkColorScheme(
+    primary = Blue80,
+    onPrimary = Blue20,
+    primaryContainer = Blue30,
+    onPrimaryContainer = Blue90,
+    inversePrimary = Blue40,
+    secondary = DarkBlue80,
+    onSecondary = DarkBlue20,
+    secondaryContainer = DarkBlue30,
+    onSecondaryContainer = DarkBlue90,
+    tertiary = Purple80,
+    onTertiary = Purple20,
+    tertiaryContainer = Purple30,
+    onTertiaryContainer = Purple90,
+    error = Red80,
+    onError = Red20,
+    errorContainer = Red30,
+    onErrorContainer = Red90,
+    background = Grey10,
+    onBackground = Grey90,
+    surface = Green30,
+    onSurface = Green80,
+    inverseSurface = Grey90,
+    inverseOnSurface = Grey10,
+    surfaceVariant = Green30,
+    onSurfaceVariant = Green80,
+    outline = Green80
 )
 
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
+private val LightColorPalette = lightColorScheme(
+    primary = Blue40,
     onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-     */
+    primaryContainer = Blue90,
+    onPrimaryContainer = Blue10,
+    inversePrimary = Blue80,
+    secondary = DarkBlue40,
+    onSecondary = Color.White,
+    secondaryContainer = DarkBlue90,
+    onSecondaryContainer = DarkBlue10,
+    tertiary = Purple40,
+    onTertiary = Purple10,
+    tertiaryContainer = Purple90,
+    onTertiaryContainer = Purple10,
+    error = Red40,
+    onError = Color.White,
+    errorContainer = Red90,
+    onErrorContainer = Red10,
+    background = Grey99,
+    onBackground = Grey10,
+    surface = Green90,
+    onSurface = Green30,
+    inverseSurface = Grey20,
+    inverseOnSurface = Grey95,
+    surfaceVariant = Green90,
+    onSurfaceVariant = Green30,
+    outline = Green50
 )
 
 @Composable
-fun BankdTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
+fun BankdTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val useDynamicColors = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+    val colors = when {
+        useDynamicColors && darkTheme -> dynamicDarkColorScheme(LocalContext.current)
+        useDynamicColors && !darkTheme -> dynamicLightColorScheme(LocalContext.current)
+        darkTheme -> DarkColorPalette
+        else -> LightColorPalette
     }
 
     MaterialTheme(
-        colors = colors,
-        typography = Typography,
+        colorScheme = colors,
+        typography = oxygenTypography,
         shapes = Shapes,
         content = content
     )
--- app/src/main/java/io/osemwota/bankd/ui/theme/Type.kt
+++ app/src/main/java/io/osemwota/bankd/ui/theme/Type.kt
@@ -1,14 +1,16 @@
 package io.osemwota.bankd.ui.theme
 
-import androidx.compose.material.Typography
+import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import io.osemwota.bankd.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    body1 = TextStyle(
+    bodyMedium = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
@@ -25,4 +27,24 @@ val Typography = Typography(
         fontSize = 12.sp
     )
      */
+)
+
+private val oxygen = FontFamily(
+    Font(R.font.oxygen_regular),
+    Font(R.font.oxygen_light, FontWeight.Light),
+    Font(R.font.oxygen_bold, FontWeight.Bold)
+)
+
+val oxygenTypography = Typography(
+//    defaultFontFamily = oxygen,
+    bodyMedium = TextStyle(
+        fontFamily = oxygen,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+//    button = TextStyle(
+//        fontFamily = oxygen,
+//        fontWeight = FontWeight.W500,
+//        fontSize = 12.sp
+//    )
 )
